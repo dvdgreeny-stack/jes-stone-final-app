5cfc028fd8ac94c9487ef4f84d0305b0bc76d6b0
dvdgreeny-stack <dvdgreeny@gmail.com>
2025-12-12 12:30:34 -0600

    Update apiService.ts

--- services/apiService.ts
+++ services/apiService.ts
@@ -6,8 +6,35 @@ const DEMO_COMPANIES: Company[] = [
         id: 'demo-co', 
         name: 'Knightvest (Demo)', 
         properties: [
-            { id: 'kv-1', name: 'The Arts at Park Place', address: '1301 W Park Blvd, Plano, TX 75075' },
-            { id: 'kv-2', name: 'Canyon Creek', address: '2000 Custer Rd, Richardson, TX 75080' }
+            { id: 'kv-1', name: 'Fitzhugh Townhomes', address: '2104 N Fitzhugh Ave Dallas,TX 75204' },
+            { id: 'kv-2', name: 'The Lofts at West 7th', address: '929 Norwood St Fort Worth,TX 76107' },
+            { id: 'kv-3', name: 'Archer Medical District', address: '2140 Medical District Dr Dallas,TX 75235' },
+            { id: 'kv-4', name: 'Benton', address: '19002 Dallas Pkwy Dallas,TX 75287' },
+            { id: 'kv-5', name: 'Cypress', address: '4690 Eldorado Pkwy Mckinney,TX 75070' },
+            { id: 'kv-6', name: 'Dorian and Encore', address: '8401 Memorial Lane Plano,TX 75024' },
+            { id: 'kv-7', name: 'The Encore', address: '4700 Tribeca Lane Plano,TX 75024' },
+            { id: 'kv-8', name: 'Trinity Bluff & Trinity District', address: '701 East Bluff Street Fort Worth,TX 76102' },
+            { id: 'kv-9', name: 'Belclaire', address: '5600 SMU Boulevard Dallas,TX 75206' },
+            { id: 'kv-10', name: 'Remi', address: '2217 Ivan St Dallas,TX 75201' },
+            { id: 'kv-11', name: 'St. James', address: '2820 McKinnon Street Dallas,TX 75201' },
+            { id: 'kv-12', name: 'The Berkeley', address: '2001 Park Hill Dr Fort Worth,TX 76110' },
+            { id: 'kv-13', name: 'The Standard', address: '5920  E University Blvd Dallas,TX 75206' },
+            { id: 'kv-14', name: 'Vail Cliffside', address: '1635 Jefferson Cliffs Way Arlington,TX 76006' },
+            { id: 'kv-15', name: 'Aberdeen at Bellmar', address: '10843 N Central Expy Dallas,TX 75231' },
+            { id: 'kv-16', name: 'Avondale Parc at Bellmar', address: '10830 Stone Canyon Rd Dallas,TX 75230' },
+            { id: 'kv-17', name: 'Hadley at Bellmar', address: '10640 Steppington Dallas,TX 75230' },
+            { id: 'kv-18', name: 'Madison at Bellmar', address: '10501 Steppington Dallas,TX 75230' },
+            { id: 'kv-19', name: 'Seville at Bellmar', address: '10651 Steppington Dallas,TX 75230' },
+            { id: 'kv-20', name: 'Kade', address: '3301 Hudnall St Dallas,TX 75235' },
+            { id: 'kv-21', name: 'Lauren', address: '7301 Oakmont Blvd Fort Worth,TX 76132' },
+            { id: 'kv-22', name: 'Carrara at Cole', address: '4649 Cole Dallas,TX 75205' },
+            { id: 'kv-23', name: 'Maddox Apartments', address: '2660 N Haskell Ave Dallas,TX 75204' },
+            { id: 'kv-24', name: 'Mission Eagle Pointe', address: '325 S Jupiter Rd Allen,TX 75002' },
+            { id: 'kv-25', name: 'Slate at Cole', address: '4650 Cole Dallas,TX 75205' },
+            { id: 'kv-26', name: 'St. Martin', address: '9425 Rolater Rd Frisco,TX 75035' },
+            { id: 'kv-27', name: 'The Halston', address: '8850 Ferguson Rd Dallas,TX 75228' },
+            { id: 'kv-28', name: 'Vantage Point', address: '10700 Woodmeadow Pkwy Dallas,TX 75228' }
+            
         ] 
     }
 ];
